9a00998b2eadff26f550e156aee42e0ffee8a5d6
ald.dev <mis@alder.com.tw>
2016-08-12 18:09:26 +0800

    add dps List pagination

--- dpsed/views.py
+++ dpsed/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import WorkOrder
 from .forms import WorkOrderForm
@@ -9,7 +10,19 @@ from .forms import WorkOrderForm
 
 def dps_list(request):
     title = "工單列表"
-    queryset = WorkOrder.objects.all()
+    queryset_list = WorkOrder.objects.all()
+
+    paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
 
     return render(request, "dpsed_list.html", locals())
 
