191e35946a9d2dd3803991cc3650ebaf2caad8d5
ald.dev <mis@alder.com.tw>
2016-08-15 18:51:21 +0800

    set bootstrap3 to  static file

--- dpsed/forms.py
+++ dpsed/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from django.forms import ModelForm, Textarea, CharField
 from .models import WorkOrder
 
 class WorkOrderForm(forms.ModelForm):
     class Meta:
         model = WorkOrder
         #fields = ["category", "zmms", "manage_memo" ]
-        exclude = ('create_at','modify' ,)
+        exclude = ('create_at','modify' ,'zmms','material_ready_date','estimated_finish',)
+        widgets = {
+            'customer': forms.TextInput(attrs={'size':20, 'title':'Customer Number', }) ,
+            'product': forms.TextInput(attrs={'size':20, 'title':'Product Number', }) ,
+
+        }
